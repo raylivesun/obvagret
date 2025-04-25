582b49925a6c6b8bf82fba926827e1bc5a8a7e87
raylivesun <162396999+raylivesun@users.noreply.github.com>
2025-04-25 12:58:39 -0300

    the main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <iterator>
+#include <map>
+#include <set>
+#include <list>
+#include <utility>
+#include <functional>
+#include <memory>
+using namespace std;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
